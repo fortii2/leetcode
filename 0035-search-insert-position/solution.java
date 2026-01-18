0902761b50c74a174d5522c6352ac9aee024901a
forty2 <hereisb612@gmail.com>
2026-01-18 22:46:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.9 MB (29.11%)

---
+++ 0035-search-insert-position/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    // [x, y]
+    public int searchInsert(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (nums[middle] < target) {
+                left = middle + 1;
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                return middle;
+            }
+        }
+
+        return left;
+    }
+}
