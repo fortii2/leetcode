20f7dd00cfe6b3aa28ae01ff49de136cfeb47b85
forty2 <hereisb612@gmail.com>
2026-01-19 20:01:09 +0000

    [LeetCode Sync] Runtime - 2 ms (91.72%), Memory - 48.1 MB (6.92%)

--- 0283-move-zeroes/solution.java
+++ 0283-move-zeroes/solution.java
@@ -1,45 +1,16 @@
-//Given an integer array nums, move all 0's to the end of it while maintaining 
-//the relative order of the non-zero elements. 
-//
-// Note that you must do this in-place without making a copy of the array. 
-//
-// 
-// Example 1: 
-// Input: nums = [0,1,0,3,12]
-//Output: [1,3,12,0,0]
-// 
-// Example 2: 
-// Input: nums = [0]
-//Output: [0]
-// 
-// 
-// Constraints: 
-//
-// 
-// 1 <= nums.length <= 10⁴ 
-// -2³¹ <= nums[i] <= 2³¹ - 1 
-// 
-//
-// 
-//Follow up: Could you minimize the total number of operations done?
-//
-// Related Topics Array Two Pointers 👍 18841 👎 566
-
-
-//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void moveZeroes(int[] nums) {
-        int p = 0;
+        int fast = 0, slow = 0;
 
-        for (int q = 0; q < nums.length; q++) {
-            if (nums[q] != 0) {
-                int temp = nums[p];
-                nums[p] = nums[q];
-                nums[q] = temp;
-                p++;
+        while (fast < nums.length) {
+            if (nums[fast] != 0) {
+                nums[slow++] = nums[fast];
             }
+            fast++;
+        }
+
+        while (slow < nums.length) {
+            nums[slow++] = 0;
         }
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
-
