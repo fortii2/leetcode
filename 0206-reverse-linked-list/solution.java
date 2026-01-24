72c921db4a55cbc9d2f4f0d8d37a110c9464aa97
forty2 <hereisb612@gmail.com>
2026-01-24 19:39:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.3 MB (49.83%)

--- 0206-reverse-linked-list/solution.java
+++ 0206-reverse-linked-list/solution.java
@@ -1,15 +1,14 @@
-    class Solution {
-        public ListNode reverseList(ListNode head) {
-            if (head == null || head.next == null) {
-                return head;
-            }
-
-            ListNode result = reverseList(head.next);
-
-            head.next.next = head;
-            head.next = null;
-
-            return result;
-
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
         }
+
+        ListNode newHead = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
     }
+}
