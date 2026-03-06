5948563be1889d9123d2e20b4c3be9182d6703db
forty2 <hereisb612@gmail.com>
2026-03-06 15:56:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.3 MB (14.65%)

---
+++ 0144-binary-tree-preorder-traversal/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public static void helper(TreeNode root, List<Integer> res){
+        if (root == null) {
+            return;
+        }
+
+        res.add(root.val);
+        helper(root.left, res);
+        helper(root.right, res);
+    }
+}
