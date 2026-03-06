f6923fa3a51158100b439d9908d5db94be83f170
forty2 <hereisb612@gmail.com>
2026-03-06 03:29:05 +0000

    [LeetCode Sync] Runtime - 14 ms (69.80%), Memory - 47.2 MB (95.69%)

--- 0347-top-k-frequent-elements/solution.java
+++ 0347-top-k-frequent-elements/solution.java
@@ -1,26 +1,25 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> minHeap =
-                new PriorityQueue<>((x, y) -> (x.getValue() - y.getValue()));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair2[1] - pair1[1]);
 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            minHeap.offer(entry);
-
-            if (minHeap.size() > k) {
-                minHeap.poll();
-            }
+        for (Map.Entry<Integer,Integer> entrySet : map.entrySet()) {
+            int[] temp = new int[]{entrySet.getKey(), entrySet.getValue()};
+            pq.offer(temp);
         }
 
-        int[] integers = new int[k];
-        for (int i = 0; i < k; i++) {
-            integers[i] = minHeap.poll().getKey();
+        int[] res = new int[k];
+        int idx = 0;
+
+        while (idx < k) {
+            res[idx++] = pq.poll()[0];
         }
 
-        return integers;
+        return res;
     }
 }
