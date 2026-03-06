e9ca00a64ec5d5d7813c0ff0fb9097ea873a26c3
forty2 <hereisb612@gmail.com>
2026-03-06 19:18:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.4 MB (99.99%)

---
+++ 0104-maximum-depth-of-binary-tree/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return helper(root);
+    }
+
+    public static int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(helper(root.left), helper(root.right));
+    }
+}
