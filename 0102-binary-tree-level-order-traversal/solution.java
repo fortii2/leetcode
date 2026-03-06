1bc8655b4e4cad200685920271304d5463572167
forty2 <hereisb612@gmail.com>
2026-03-06 17:50:10 +0000

    [LeetCode Sync] Runtime - 1 ms (95.89%), Memory - 46.6 MB (90.77%)

---
+++ 0102-binary-tree-level-order-traversal/solution.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=102 lang=java
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+
+// @lc code=start
+
+import java.util.*;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        if (root == null) {
+            return res;
+        }
+
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode cur = q.poll();
+                level.add(cur.val);
+
+                if (cur.left != null) {
+                    q.offer(cur.left);
+                }
+
+                if (cur.right != null) {
+                    q.offer(cur.right);
+                }
+            }
+
+            res.add(level);
+        }
+
+        return res;
+    }
+}
+// @lc code=end
+
