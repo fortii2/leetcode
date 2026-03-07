814ea5af25904d1ea327ee5d07065adc5095ca8a
forty2 <hereisb612@gmail.com>
2026-03-07 22:13:09 +0000

    [LeetCode Sync] Runtime - 16 ms (89.29%), Memory - 93.7 MB (76.94%)

--- 0217-contains-duplicate/solution.java
+++ 0217-contains-duplicate/solution.java
@@ -3,9 +3,8 @@ class Solution {
         Set<Integer> set = new HashSet<>();
 
         for (int num : nums) {
-            if (!set.add(num)) {
-                return true;
-            }
+            if (set.contains(num)) return true;
+            set.add(num);
         }
 
         return false;
