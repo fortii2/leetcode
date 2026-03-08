1e585c572a3da6b191c3fc0c7893a7627729079e
forty2 <hereisb612@gmail.com>
2026-03-08 05:15:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.8 MB (90.44%)

--- 0054-spiral-matrix/solution.java
+++ 0054-spiral-matrix/solution.java
@@ -1,47 +1,39 @@
-/*
- * @lc app=leetcode id=54 lang=java
- *
- * [54] Spiral Matrix
- */
-
-// @lc code=start
-
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         int top = 0;
         int left = 0;
-        int right = matrix[0].length - 1;
         int bottom = matrix.length - 1;
+        int right = matrix[0].length - 1;
 
         List<Integer> res = new ArrayList<>();
 
-        while (left <= right && top <= bottom) {
-            for (int j = left; j <= right; j++) {
-                res.add(matrix[top][j]);
+        while (top <= bottom && left <= right) {
+
+            // left to right
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
             }
             top++;
 
-            for (int i = top; i <= bottom; i++) {
-                res.add(matrix[i][right]);
+            // top to bottom
+            for (int j = top; j <= bottom; j++) {
+                res.add(matrix[j][right]);
 
             }
             right--;
 
+            // right to left
             if (top <= bottom) {
-                for (int j = right; j >= left; j--) {
-                    res.add(matrix[bottom][j]);
-
+                for (int k = right; k >= left; k--) {
+                    res.add(matrix[bottom][k]);
                 }
                 bottom--;
             }
 
+            // bottom to top
             if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    res.add(matrix[i][left]);
-
+                for (int l = bottom; l >= top; l--) {
+                    res.add(matrix[l][left]);
                 }
                 left++;
             }
@@ -50,5 +42,3 @@ class Solution {
         return res;
     }
 }
-// @lc code=end
-
