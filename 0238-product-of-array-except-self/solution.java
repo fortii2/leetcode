9f30cb3ce23e898fc446e99ec49724c1e94cc349
forty2 <hereisb612@gmail.com>
2026-03-08 17:54:43 +0000

    [LeetCode Sync] Runtime - 2 ms (92.93%), Memory - 64.3 MB (93.21%)

--- 0238-product-of-array-except-self/solution.java
+++ 0238-product-of-array-except-self/solution.java
@@ -1,62 +1,27 @@
-//Given an integer array nums, return an array answer such that answer[i] is 
-//equal to the product of all the elements of nums except nums[i]. 
-//
-// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit 
-//integer. 
-//
-// You must write an algorithm that runs in O(n) time and without using the 
-//division operation. 
-//
-// 
-// Example 1: 
-// Input: nums = [1,2,3,4]
-//Output: [24,12,8,6]
-// 
-// Example 2: 
-// Input: nums = [-1,1,0,-3,3]
-//Output: [0,0,9,0,0]
-// 
-// 
-// Constraints: 
-//
-// 
-// 2 <= nums.length <= 10⁵ 
-// -30 <= nums[i] <= 30 
-// The input is generated such that answer[i] is guaranteed to fit in a 32-bit 
-//integer. 
-// 
-//
-// 
-// Follow up: Can you solve the problem in O(1) extra space complexity? (The 
-//output array does not count as extra space for space complexity analysis.) 
-//
-// Related Topics Array Prefix Sum 👍 25254 👎 1643
-
-
-//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] L = new int[nums.length];
-        int[] R = new int[nums.length];
-        int[] out = new int[nums.length];
 
-        L[0] = 1;
-        for (int i = 1; i < L.length; i++) {
-            L[i] = L[i - 1] * nums[i - 1];
+        int n = nums.length;
+        int[] res = new int[n];
+
+        int[] prefix = new int[n];
+        prefix[0] = 1;
+
+        int suffix = 1;
+
+        // 2,2,3,4
+        // 1 2 4 12 prefix
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
         }
 
-        R[nums.length - 1] = 1;
-        for (int i = R.length - 2; i >= 0; i--) {
-            R[i] = R[i + 1] * nums[i + 1];
+        // 2, 2, 3, 4
+        //              1 suffix
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] = prefix[i] * suffix;
+            suffix *= nums[i];
         }
 
-        for (int i = 0; i < out.length; i++) {
-            out[i] = L[i] * R[i];
-        }
-
-        return out;
-
+        return res;
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
-
