e28b5ee878b1f8cbe57939194e6ed045ab38b478
forty2 <hereisb612@gmail.com>
2026-03-08 05:49:50 +0000

    [LeetCode Sync] Runtime - 14 ms (69.80%), Memory - 47.7 MB (49.61%)

--- 0347-top-k-frequent-elements/solution.java
+++ 0347-top-k-frequent-elements/solution.java
@@ -1,23 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair2[1] - pair1[1]);
+        Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
 
-        for (Map.Entry<Integer,Integer> entrySet : map.entrySet()) {
-            int[] temp = new int[]{entrySet.getKey(), entrySet.getValue()};
-            pq.offer(temp);
+        for (Map.Entry entry : map.entrySet()) {
+            pq.offer(entry);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
         }
 
         int[] res = new int[k];
-        int idx = 0;
-
-        while (idx < k) {
-            res[idx++] = pq.poll()[0];
+        for (int i = k - 1; i >= 0; i--) {
+            res[i] = pq.poll().getKey();
         }
 
         return res;
