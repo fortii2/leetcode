bc9369ce1f0997e6b4bcb17b545b9ffb8226b436
forty2 <hereisb612@gmail.com>
2026-03-08 03:33:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.5 MB (58.45%)

--- 0019-remove-nth-node-from-end-of-list/solution.java
+++ 0019-remove-nth-node-from-end-of-list/solution.java
@@ -1,22 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+
         ListNode sentinel = new ListNode();
         sentinel.next = head;
-
+        
         ListNode fast = sentinel;
         ListNode slow = sentinel;
 
-        int moveFast = n + 1;
-        while (moveFast-- > 0) {
+        for (int i = 0; i <= n; i++) {
             fast = fast.next;
         }
 
+        // sentinel 1 null
+
         while (fast != null) {
-            fast = fast.next;
             slow = slow.next;
+            fast = fast.next;
         }
 
         slow.next = slow.next.next;
+
         return sentinel.next;
+
     }
 }
