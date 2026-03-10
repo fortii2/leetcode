2927495cd6cb2e9b52bdd19f3a7ce45c81f52e14
forty2 <hereisb612@gmail.com>
2026-03-10 03:18:52 +0000

    [LeetCode Sync] Runtime - 4 ms (46.00%), Memory - 52.1 MB (71.75%)

---
+++ 0200-number-of-islands/solution.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int res = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    res++;
+                    dfs(grid, visited, i, j);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public void dfs(char[][] grid, boolean[][] visited, int i, int j) {
+
+        if (i < 0 || i >= grid.length) {
+            return;
+        }
+
+        if (j < 0 || j >= grid[0].length) {
+            return;
+        }
+
+        if (grid[i][j] == '0') {
+            return;
+        }
+
+        if (visited[i][j]) {
+            return;
+        }
+
+        visited[i][j] = true;
+
+        dfs(grid, visited, i - 1, j);
+        dfs(grid, visited, i + 1, j);
+        dfs(grid, visited, i, j - 1);
+        dfs(grid, visited, i, j + 1);
+    }
+}
