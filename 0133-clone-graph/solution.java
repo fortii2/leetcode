b222c84493f00865a12ffa47618567d541f4814e
forty2 <hereisb612@gmail.com>
2026-03-10 03:09:51 +0000

    [LeetCode Sync] Runtime - 24 ms (92.38%), Memory - 44.4 MB (22.64%)

---
+++ 0133-clone-graph/solution.java
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+
+    // original, cloned
+    HashMap<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+
+        if (node == null) {
+            return null;
+        }
+
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+
+        Node cloned = new Node(node.val);
+        map.put(node, cloned);
+
+        for (Node n : node.neighbors) {
+            cloned.neighbors.add(cloneGraph(n));
+        }
+
+        return cloned;
+
+    }
+}
