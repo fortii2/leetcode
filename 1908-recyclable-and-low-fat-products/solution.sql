6194b1cbdb9a82e6de5dcbb5de21de4534deee0d
forty2 <hereisb612@gmail.com>
2026-08-26 19:21:57 +0000

    [LeetCode Sync] Runtime - 610 ms (46.03%), Memory - 0.0B (100.00%)

--- 1908-recyclable-and-low-fat-products/solution.sql
+++ 1908-recyclable-and-low-fat-products/solution.sql
@@ -1,7 +1,5 @@
-select
-    product_id
-from
-    Products
-where
-    low_fats = 'Y'
+# Write your MySQL query statement below
+select product_id
+from Products
+where low_fats = 'Y'
     and recyclable = 'Y'
