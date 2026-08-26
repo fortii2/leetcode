c4e3f047de02ea68b6b039ad220f75327a0b4d21
forty2 <hereisb612@gmail.com>
2026-08-26 21:18:53 +0000

    [LeetCode Sync] Runtime - 254 ms (79.92%), Memory - 0.0B (100.00%)

--- 1801-average-time-of-process-per-machine/solution.sql
+++ 1801-average-time-of-process-per-machine/solution.sql
@@ -1,12 +1,8 @@
-select
-    p.machine_id,
-    round(avg(q.timestamp - p.timestamp), 3) processing_time
-from
-    Activity p
-    join Activity q 
+select p.machine_id, round(avg(q.timestamp - p.timestamp), 3) as processing_time
+from Activity p
+join Activity q
     on p.machine_id = q.machine_id
     and p.process_id = q.process_id
-    and p.activity_type = "start"
-    and q.activity_type = "end"
-group by
-    p.machine_id
+    and p.activity_type = 'start'
+    and q.activity_type = 'end'
+group by p.machine_id
