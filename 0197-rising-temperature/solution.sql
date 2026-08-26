37f415f4a956d3bdbc9d7fc4c6798ba882b5e33a
forty2 <hereisb612@gmail.com>
2026-08-26 20:59:42 +0000

    [LeetCode Sync] Runtime - 376 ms (97.90%), Memory - 0.0B (100.00%)

--- 0197-rising-temperature/solution.sql
+++ 0197-rising-temperature/solution.sql
@@ -1,5 +1,14 @@
-select p.id
-from Weather p
-join Weather q
-on datediff(p.recordDate, q.recordDate) = 1
-where p.temperature > q.temperature
+# Write your MySQL query statement below
+with weather_with_previous_year as (
+    select id, 
+        recordDate,
+        temperature,
+        lag(recordDate) over (order by recordDate) as pre_recordDate,
+        lag(temperature) over (order by recordDate) as pre_temperature
+    from Weather
+)
+
+select id
+from weather_with_previous_year
+where temperature > pre_temperature
+    and datediff(recordDate, pre_recordDate) = 1
