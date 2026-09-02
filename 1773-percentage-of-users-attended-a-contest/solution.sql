32ec01e7c2662b022f8e57a3c564aa916133960c
forty2 <hereisb612@gmail.com>
2026-09-02 16:51:52 +0000

    [LeetCode Sync] Runtime - 1197 ms (29.69%), Memory - 0.0B (100.00%)

--- 1773-percentage-of-users-attended-a-contest/solution.sql
+++ 1773-percentage-of-users-attended-a-contest/solution.sql
@@ -1,16 +1,8 @@
 # Write your MySQL query statement below
-select contest_id, ROUND(count(user_id) / (select count(*) from Users) * 100, 2) as percentage
+select contest_id, 
+    round(
+        count(*) / (select count(*) from Users) * 100.0,
+        2) as percentage
 from Register
 group by contest_id
-order by percentage desc, contest_id asc
-
-
-
--- +-------------+---------+
--- | Column Name | Type    |
--- +-------------+---------+
--- | u.user_id     | int     |
--- | u.user_name   | varchar |
--- | r.contest_id  | int     |
--- | r.user_id     | int     |
--- +-------------+---------+
+order by percentage DESC, contest_id ASC
