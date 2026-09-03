ea090e3dad4c012b820148339d0c8f331148fce2
forty2 <hereisb612@gmail.com>
2026-09-03 19:29:27 +0000

    [LeetCode Sync] Runtime - 833 ms (23.56%), Memory - 0.0B (100.00%)

--- 1292-immediate-food-delivery-ii/solution.sql
+++ 1292-immediate-food-delivery-ii/solution.sql
@@ -1,7 +1,16 @@
 # Write your MySQL query statement below
-select ROUND(sum(if(customer_pref_delivery_date=order_date, 1, 0)) / count(*) * 100, 2) as immediate_percentage
-from (
+# each customer's first order date.
+
+with first_order_date as (
     select *,
-    row_number() over(partition by customer_id order by order_date asc) as rn
-    from Delivery) t
+        row_number() over (
+            partition by customer_id 
+            order by order_date ASC
+        ) as rn
+    from Delivery
+)
+
+select 
+    round(sum(order_date = customer_pref_delivery_date) / count(*) * 100.0, 2) as immediate_percentage
+from first_order_date
 where rn = 1
