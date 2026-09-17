7df4f2507bfe67d07657a9fdd63c9b5e01f9b271
forty2 <hereisb612@gmail.com>
2026-09-17 01:19:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.9 MB (44.60%)

---
+++ 0035-search-insert-position/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        # [,)
+        p, q = 0, len(nums)
+
+        while p < q:
+            mid = (p + q) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                p = mid + 1
+            else:
+                q = mid
+        
+        return p
